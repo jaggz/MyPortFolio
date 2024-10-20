bbe49b8db18a60b0964aa1b59a1804463a554eed
JSD <js.dohil@gmail.com>
2024-10-19 23:30:06 -0700

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(){
     return (
             <div className="col-12 text-center  ">
                 <a className="icon-link icon-link-hover mx-2 text-dark fs-1" style={style} href="https://github.com/jaggz"><i className="bi bi-github"></i></a>
-                <a className="icon-link icon-link-hover mx-2 text-dark fs-1" style={style} href=""><i className="bi bi-linkedin"></i></a>
+                <a className="icon-link icon-link-hover mx-2 text-dark fs-1" style={style} href="https://www.linkedin.com/in/jagdeep-dohil-80bb73269"><i className="bi bi-linkedin"></i></a>
                 <a className="icon-link icon-link-hover mx-2 text-dark fs-1" style={style} href=""><i className="bi bi-twitter"></i></a>
             </div>
 
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,10 @@
 export default function Project() {
     const projects = [
-                {name:"MyPortFolio",gitRepo:'https://github.com/jaggz/MyPortFolio',customclass:'portfolio'},
-                {name:"Weather ForeCast",gitRepo:'https://github.com/jaggz/weatherForcast',customclass:'weatherForcast'},
-                {name:"Build Vehicle CLI",gitRepo:'https://github.com/jaggz/BuildVehicle',customclass:'buildVehicle'},
-                {name:"ReadME Generator",gitRepo:'https://github.com/jaggz/Gen-ReadMe',customclass:'readmeGen'},
-                {name:"Employee Tracker",gitRepo:'https://github.com/jaggz/TrackEmp',customclass:'trackemp'},
+                {name:"MyPortFolio",gitRepo:'https://github.com/jaggz/MyPortFolio',customclass:'portfolio',deployedLink:'https://earnest-sprinkles-335372.netlify.app/'},
+                {name:"Weather ForeCast",gitRepo:'https://github.com/jaggz/weatherForcast',customclass:'weatherForcast',deployedLink:'https://weatherforcast-m4s0.onrender.com/'},
+                {name:"Build Vehicle CLI",gitRepo:'https://github.com/jaggz/BuildVehicle',customclass:'buildVehicle',deployedLink:"#"},
+                {name:"ReadME Generator",gitRepo:'https://github.com/jaggz/Gen-ReadMe',customclass:'readmeGen',deployedLink:"#"},
+                {name:"Employee Tracker",gitRepo:'https://github.com/jaggz/TrackEmp',customclass:'trackemp',deployedLink:'#'},
     ];
 
     return (
@@ -21,7 +21,8 @@ export default function Project() {
                             <figure className={`flex-item ${project.customclass} rounded shadow-lg`}>
                                 <div className="rounded opacity-50">
                                     <h3>{project.name}</h3>
-                                    <a href={project.gitRepo} className="text-success"><i className="bi bi-git">Git Repository</i></a>
+                                    <a href={project.gitRepo} className="text-light"><i className="bi bi-git">Git Repository</i></a><br/>
+                                    <a href={project.deployedLink} className="text-light"><i className="bi bi-browser-chrome">WebPage</i></a>
                                 </div>
                             </figure>
                             </div>
